f9fe05782d6d18d5be4e20cf4cdb079edd6cb4e8
mariamsavi <mseyedemousavi@gmail.com>
2025-03-04 11:19:53 +0100

    Base

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace AnimalMatchingGame;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void SpillIgjenButton_Clicked(object sender, EventArgs e)
+	{
+	}
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+
+}
+
