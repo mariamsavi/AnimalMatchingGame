cb3109da1d2364caff907eef489b84c821af0466
mariamsavi <mseyedemousavi@gmail.com>
2025-03-10 09:48:16 +0100

    Finished up the buttons

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -10,12 +10,63 @@ public partial class MainPage : ContentPage
 
 	private void SpillIgjenButton_Clicked(object sender, EventArgs e)
 	{
+		AnimalButtons.IsVisible = true;
+		SpillIgjenButton.IsVisible = false;
+
+		List<string> AnimalEmoji = [
+			"🐭", "🐭",
+			"🐷","🐷",
+			"🐶","🐶",
+			"🐮", "🐮",
+			"🐨","🐨",
+			"🕊️","🕊️",
+			"🐲","🐲",
+			"🐠","🐠",
+		];
+		foreach (var button in AnimalButtons.Children.OfType<Button>())
+		{
+			int index = Random.Shared.Next(AnimalEmoji.Count);
+			string nextEmoji = AnimalEmoji[index];
+			button.Text = nextEmoji;
+			AnimalEmoji.RemoveAt(index);
+		}
 	}
+
+	Button lastClicked;
+	bool findingMatch = false;
+	int matchesFound;
 	private void Button_Clicked(object sender, EventArgs e)
 	{
+		if (sender is Button buttonClicked)
+		{
 
+			if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+			{
+				buttonClicked.BackgroundColor = Color.FromArgb("#E085B0");
+				lastClicked = buttonClicked;
+				findingMatch = true;
+			}
+			else
+			{
+				if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text))
+				{
+					matchesFound++;
+					lastClicked.Text = " ";
+					buttonClicked.Text = " ";
+				}
+				lastClicked.BackgroundColor = Color.FromArgb("#FF98C8");
+				buttonClicked.BackgroundColor = Color.FromArgb("#FF98C8");
+				findingMatch = false;
+			}
+		}
+		if (matchesFound == 8)
+		{
+			matchesFound = 0;
+			AnimalButtons.IsVisible = false;
+			SpillIgjenButton.IsVisible = true;
+
+
+		}
 	}
 
-
 }
-
